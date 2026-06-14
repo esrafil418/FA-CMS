a4ac79c421f0cdfbf554390d88555fec4f316920
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-14 13:52:25 +0330

    fix: table warning

--- src/components/ProductsTable/ProductsTable.jsx
+++ src/components/ProductsTable/ProductsTable.jsx
@@ -13,42 +13,45 @@ export default function ProductsTable() {
 	return (
 		<>
 			<table className="products-table">
-				<tr className="products-table-heading-tr">
-					<th>عکس</th>
-					<th>اسم</th>
-					<th>قیمت</th>
-					<th>موجودی</th>
-				</tr>
+				<thead>
+					<tr className="products-table-heading-tr">
+						<th>عکس</th>
+						<th>اسم</th>
+						<th>قیمت</th>
+						<th>موجودی</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr className="products-table-tr">
+						<td>
+							<img
+								src="/images/Seong.webp"
+								alt="example"
+								className="products-table-img"
+							/>
+						</td>
 
-				<tr className="products-table-tr">
-					<td>
-						<img
-							src="/images/Seong.webp"
-							alt="example"
-							className="products-table-img"
-						/>
-					</td>
+						<td className="">نمونه تست</td>
+						<td className="">100 هزار تومان</td>
+						<td className="">45</td>
 
-					<td className="">نمونه تست</td>
-					<td className="">100 هزار تومان</td>
-					<td className="">45</td>
-
-					<td>
-						<button type="button" className="products-table-btn">
-							جزییات
-						</button>
-						<button
-							type="button"
-							className="products-table-btn"
-							onClick={() => setIsShowModal(true)}
-						>
-							حذف
-						</button>
-						<button type="button" className="products-table-btn">
-							ویرایش
-						</button>
-					</td>
-				</tr>
+						<td>
+							<button type="button" className="products-table-btn">
+								جزییات
+							</button>
+							<button
+								type="button"
+								className="products-table-btn"
+								onClick={() => setIsShowModal(true)}
+							>
+								حذف
+							</button>
+							<button type="button" className="products-table-btn">
+								ویرایش
+							</button>
+						</td>
+					</tr>
+				</tbody>
 			</table>
 
 			{isShowModal && (
