97a408df6d71cf793c56a8aeb9fd65481b236024
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-14 10:07:03 +0330

    refactor: routes structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Products from "./components/Products/Products";
-import Comments from "./components/Comments/Comments";
-import Users from "./components/Users/Users";
-import Orders from "./components/Orders/Orders";
-import Off from "./components/Off/Off";
+import { useRoutes } from "react-router-dom";
+import routes from "./routes";
 
 export default function App() {
+	const router = useRoutes(routes);
 	return (
 		<>
 			<Sidebar />
-
 			<div className="main">
 				<Header />
 
-				<Routes>
-					<Route path="/products" element={<Products />} />
-					<Route path="/comments" element={<Comments />} />
-					<Route path="/users" element={<Users />} />
-					<Route path="/orders" element={<Orders />} />
-					<Route path="/off" element={<Off />} />
-				</Routes>
+				{router}
 			</div>
 		</>
 	);
