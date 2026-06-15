52ae971919e72a766d1fbcc3bbd5ef5c5a2c8103
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-15 18:19:31 +0330

    feat: implement Comment Section logic

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,49 +1,76 @@
 import { useEffect, useState } from "react";
 import ErrorBox from "../Error/ErrorBox";
 import "./Comments.css";
+import DetailsModal from "../DetailsModal/DetailsModal";
 
 export default function Comments() {
 	const [allComments, setAllComments] = useState([]);
+	const [isShowDetailModal, setIsShowDetailModal] = useState(false);
+	const [mainCommentBody, setMainCommentBody] = useState("");
 
 	const API_BASE_URL = "http://localhost:3000/api/comments";
 
 	useEffect(() => {
 		fetch(API_BASE_URL)
 			.then((res) => res.json())
-			.then((comments) => setAllComments(comments));
+			.then((comments) => {
+				setAllComments(comments);
+			});
 	}, []);
+
+	const closeDetailsModal = () => setIsShowDetailModal(false);
+
 	return (
 		<div>
-			<ErrorBox msg="هیچ کامنتی یافت نشد!" />
-			<table className="cms-table">
-				<thead>
-					<tr>
-						<th>اسم کاربر</th>
-						<th>محصول</th>
-						<th>کامنت</th>
-						<th>تاریخ</th>
-						<th>ساعت</th>
-					</tr>
-				</thead>
+			{allComments.length ? (
+				<table className="cms-table">
+					<thead>
+						<tr>
+							<th>اسم کاربر</th>
+							<th>محصول</th>
+							<th>کامنت</th>
+							<th>تاریخ</th>
+							<th>ساعت</th>
+						</tr>
+					</thead>
 
-				<tbody>
-					<tr>
-						<td>علی</td>
-						<td>آیفون 13</td>
-						<td>
-							<button type="button">مشاهده متن</button>
-						</td>
-						<td>1405-01-01</td>
-						<td>16:30</td>
-						<td>
-							<button type="button">حذف</button>
-							<button type="button">ویرایش</button>
-							<button type="button">پاسخ</button>
-							<button type="button">تایید</button>
-						</td>
-					</tr>
-				</tbody>
-			</table>
+					<tbody>
+						{allComments.map((comment) => (
+							<tr key={comment.id}>
+								<td>{comment.userID}</td>
+								<td>{comment.productID}</td>
+								<td>
+									<button
+										type="button"
+										onClick={() => {
+											setMainCommentBody(comment.body);
+											setIsShowDetailModal(true);
+										}}
+									>
+										مشاهده متن
+									</button>
+								</td>
+								<td>{comment.date}</td>
+								<td>{comment.hour}</td>
+								<td>
+									<button type="button">حذف</button>
+									<button type="button">ویرایش</button>
+									<button type="button">پاسخ</button>
+									<button type="button">تایید</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			) : (
+				<ErrorBox msg="هیچ کامنتی یافت نشد!" />
+			)}
+
+			{isShowDetailModal && (
+				<DetailsModal onHide={closeDetailsModal}>
+					<p className="text-modal">{mainCommentBody}</p>
+				</DetailsModal>
+			)}
 		</div>
 	);
 }
