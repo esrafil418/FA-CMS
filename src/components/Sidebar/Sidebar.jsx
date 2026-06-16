e1c844bfc8804151107947b68fcd4cd3459bf0a5
Esrafil <esrafil.s.r.man418@gmail.com>
2026-06-16 18:57:50 +0330

    refactor: Use NavLink instead of link

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import { MdProductionQuantityLimits } from "react-icons/md";
 import { BiCommentDetail } from "react-icons/bi";
 import { FiUsers } from "react-icons/fi";
 import { BsBagCheck, BsCurrencyDollar } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
 	return (
@@ -12,42 +12,30 @@ export default function Sidebar() {
 			<h1 className="sidebar-title">داشبورد ادمین - CMS FA</h1>
 
 			<ul className="sidebar-links">
-				<li>
-					<Link to="/products">
-						<AiOutlineHome className="icon" />
-						صفحه اصلی
-					</Link>
-				</li>
-				<li className="active">
-					<Link to="/products">
-						<MdProductionQuantityLimits className="icon" />
-						محصولات
-					</Link>
-				</li>
-				<li>
-					<Link to="/comments">
-						<BiCommentDetail className="icon" />
-						کامنت ها
-					</Link>
-				</li>
-				<li>
-					<Link to="/users">
-						<FiUsers className="icon" />
-						کاربران
-					</Link>
-				</li>
-				<li>
-					<Link to="/orders">
-						<BsBagCheck className="icon" />
-						سفارشات
-					</Link>
-				</li>
-				<li>
-					<Link to="/off">
-						<BsCurrencyDollar className="icon" />
-						تخفیفات
-					</Link>
-				</li>
+				<NavLink to="/products">
+					<AiOutlineHome className="icon" />
+					صفحه اصلی
+				</NavLink>
+				<NavLink to="/products">
+					<MdProductionQuantityLimits className="icon" />
+					محصولات
+				</NavLink>
+				<NavLink to="/comments">
+					<BiCommentDetail className="icon" />
+					کامنت ها
+				</NavLink>
+				<NavLink to="/users">
+					<FiUsers className="icon" />
+					کاربران
+				</NavLink>
+				<NavLink to="/orders">
+					<BsBagCheck className="icon" />
+					سفارشات
+				</NavLink>
+				<NavLink to="/off">
+					<BsCurrencyDollar className="icon" />
+					تخفیفات
+				</NavLink>
 			</ul>
 		</div>
 	);
